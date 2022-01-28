4137e36271ad72fefaa2463200f18f77dcf9f09f
GuyKroizman <guykr@wix.com>
2022-01-29 01:53:33 +0200

    add fireRate

--- Assets/Scripts/Weapons/Rifle.cs
+++ Assets/Scripts/Weapons/Rifle.cs
@@ -1,16 +1,31 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rifle : MonoBehaviour, IWeapon
 {
 	[field: SerializeField] public GameObject ammo { get; set; }
+	[field: SerializeField] public int fireRate;
+
+	bool allowFire = true;
 
 	public void Fire()
+    {
+		StartCoroutine(Shoot());
+    }
+
+	public IEnumerator Shoot()
 	{
-		Vector3 pos;
-		pos = transform.Find("Bullet Spawn").position;
-		Instantiate<GameObject>(ammo, pos, transform.rotation);
+		if (!allowFire) {
+			yield return new WaitForSeconds(0);
+		}else {
+			allowFire = false;
+
+			Vector3 pos;
+			pos = transform.Find("Bullet Spawn").position;
+			Instantiate<GameObject>(ammo, pos, transform.rotation);
+			yield return new WaitForSeconds(fireRate);
+			allowFire = true;
+	    }
 	}
 
 	public Vector3 LocateSpawnPositionOnOtherBoard()
