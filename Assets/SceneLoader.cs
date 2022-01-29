3ce434d85af860548d2f83f94268460cfb254e19
GuyKroizman <guykr@wix.com>
2022-01-29 19:54:20 +0200

    main menu

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
