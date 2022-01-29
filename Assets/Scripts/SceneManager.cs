bd4c3801aecd0b4e8e08cb7e6a2b37ce13aa057d
Guy Rahamim <guy.danino5@gmail.com>
2022-01-29 13:26:58 +0200

    Destroy bullets when leaving associated plane.

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -2,8 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public struct ManagerInfo
+{
+    public GameObject player1;
+    public GameObject player2;
+
+    public GameObject Plane1;
+    public GameObject Plane2;
+
+    public ManagerInfo(GameObject p1, GameObject p2, GameObject plane1, GameObject plane2)
+	{
+        player1 = p1;
+        player2 = p2;
+        this.Plane1 = plane1;
+        this.Plane2 = plane2;
+	}
+}
+
 public class SceneManager : MonoBehaviour
 {
+
+
     #region Singleton
 
     public static SceneManager instance;
@@ -14,6 +34,16 @@ public class SceneManager : MonoBehaviour
 
     #endregion 
 
-    public GameObject player1;
-    public GameObject player2;
+    public static GameObject player1 = GameObject.Find("Player");
+    public static GameObject player2 = GameObject.Find("player 2");
+
+    public static GameObject Plane1 = GameObject.Find("Player Plane");
+    public static GameObject Plane2 = GameObject.Find("Player Plane 2");
+
+    public static ManagerInfo GetInfo()
+	{
+        return new ManagerInfo(player1, player2, Plane1, Plane2);
+	}
+
+    
 }
